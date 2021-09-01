b45206af02502de660907a6f1a8667867e5bcbd3
XudongZhao-Iecube <67267793+XudongZhao-Iecube@users.noreply.github.com>
2021-09-01 15:42:18 +0800

    2.2 构造函数

--- Classes/Simple Class/BasicClass.m
+++ Classes/Simple Class/BasicClass.m
@@ -3,7 +3,14 @@ classdef BasicClass
       Value {mustBeNumeric}
    end
    methods
-      function r = roundOff(obj)
+       % 构造函数
+        function obj = BasicClass(val)
+            % 参数有 1 个的时候
+            if nargin == 1   
+                obj.Value = val;
+            end
+        end
+        function r = roundOff(obj)
          r = round([obj.Value],2);
       end
       function r = multiplyBy(obj,n)
---
+++ Classes/Simple Class/BasicClass_Main_Constructor.m
@@ -0,0 +1,2 @@
+a = BasicClass(pi/3)
+b = BasicClass()

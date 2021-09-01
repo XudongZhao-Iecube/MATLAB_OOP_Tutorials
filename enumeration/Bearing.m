e29dbde392eb25bf3d5e17770e47cc70f9dcb57f
XudongZhao-Iecube <67267793+XudongZhao-Iecube@users.noreply.github.com>
2021-09-01 13:40:16 +0800

    1.2 转换为超类值

---
+++ enumeration/Bearing.m
@@ -0,0 +1,9 @@
+% 转换为超类值
+classdef Bearing < uint32
+    enumeration
+        North (0)
+        East  (90)
+        South (180)
+        West  (270)     
+    end
+end
---
+++ enumeration/Bearing_Main.m
@@ -0,0 +1,10 @@
+a = Bearing.East;
+b = uint32(a);
+whos
+
+% 运行结果
+% Name       Size            Bytes  Class       Attributes
+% 
+%   a          1x1                 4  Bearing               
+%   b          1x1                 4  uint32                
+%   today      1x1                 0  WeekDays       

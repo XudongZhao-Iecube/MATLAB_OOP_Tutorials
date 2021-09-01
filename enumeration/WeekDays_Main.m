328d4a48bb493f8d1acd4049a2752265db689475
XudongZhao-Iecube <67267793+XudongZhao-Iecube@users.noreply.github.com>
2021-09-01 14:02:53 +0800

    Update WeekDays_Main.m
    基于值的枚举类的实例是唯一的

--- enumeration/WeekDays_Main.m
+++ enumeration/WeekDays_Main.m
@@ -1,2 +1,20 @@
 today = WeekDays.Tuesday;
 today
+
+% today = 
+% 
+%   WeekDays 枚举
+% 
+%     Tuesday
+
+%% 基于值的枚举类的实例是唯一的，直到该类被清除并重新加载。
+a = WeekDays.Monday;
+b = WeekDays.Monday;
+isequal(a,b)
+
+% MATLAB® 将 a 和 b 视为等价的
+% ans =
+% 
+%   logical
+% 
+%    1
